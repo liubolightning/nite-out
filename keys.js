c6a03bdf9e7c4049c8623a19f9aabd7713b798e0
bo liu <liubo0331@gmail.com>
2014-09-11 09:59:31 -0700

    Add keys.js

---
+++ keys.js
@@ -0,0 +1,18 @@
+module.exports = {
+  yelpKey: '_g2j463N3o27NnMbTgNXOw',
+  yelpSecret: 'UFBGBJftiGp8wD5uVvCNcq4Gblo',
+  yelpToken: 'IYrt2LP2k-usbvq6Za7JAHRWSX5aWVUi',
+  yelpTokenSecret: 'Of8O7VPg_iialw0jcsgcEguGf0E',
+  mysql: {
+    client: 'mysql',
+    connection: {
+      host     : '127.0.0.1',
+      user     : 'root',
+      password : '',
+      database : 'nite-out',
+      charset  : 'utf8'
+    }
+  },
+  mongodb: 'mongodb://admin:1234@ds052827.mongolab.com:52827/nite-out',
+  eventApiKey: 'TC72RKRTXUG45LEK44'
+};
